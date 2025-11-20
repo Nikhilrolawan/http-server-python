39e132c0de65fa11481b5e5e6b2093c120efb2ca
NIKHIL SAINI <nikhilsaini@NIKHILs-MacBook-Air.local>
2025-11-20 23:12:35 +0530

    handled theading

--- app/main.py
+++ app/main.py
@@ -1,5 +1,36 @@
 import socket  # noqa: F401
+import threading
 
+
+def handle_response(conn, data):
+    
+    if data.startswith("GET / HTTP/1.1"):
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\nHello, World!") # wait for client
+    elif data.startswith("GET /echo/"):
+        endpoint = data.split('/')[2][:-5]
+        conn.sendall("HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/plain\r\n"
+                    "Content-Length: {}\r\n\r\n{}"
+                    .format(len(endpoint), endpoint).encode())
+    elif data.startswith("GET /user-agent"):
+        headers = data.split("\r\n")
+        endpoint = ''
+        for h in headers:
+            if h.startswith("User-Agent:"):
+                endpoint = h[len("User-Agent:"):].strip()
+
+        conn.sendall("HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/plain\r\n"
+                    "Content-Length: {}\r\n\r\n{}"
+                    .format(len(endpoint), endpoint).encode())
+
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n") # wait for client
+
+def handle_request(conn):
+    data = conn.recv(1024).decode()
+    handle_response(conn, data)
+    conn.close()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,33 +40,10 @@ def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server is running on port 4221")
-
     while True:
+        server_socket.listen()
         conn, addr = server_socket.accept() # wait for client
-        data = conn.recv(1024).decode()
-        print(data)
-        if data.startswith("GET / HTTP/1.1"):
-            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\nHello, World!") # wait for client
-        elif data.startswith("GET /echo/"):
-            endpoint = data.split('/')[2][:-5]
-            conn.sendall("HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/plain\r\n"
-                        "Content-Length: {}\r\n\r\n{}"
-                        .format(len(endpoint), endpoint).encode())
-        elif data.startswith("GET /user-agent"):
-            headers = data.split("\r\n")
-            endpoint = ''
-            for h in headers:
-                if h.startswith("User-Agent:"):
-                    endpoint = h[len("User-Agent:"):].strip()
-
-            conn.sendall("HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/plain\r\n"
-                        "Content-Length: {}\r\n\r\n{}"
-                        .format(len(endpoint), endpoint).encode())
-
-        else:
-            conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n") # wait for client
+        threading.Thread(target=handle_request, args=(conn,)).start()
 
 
 if __name__ == "__main__":
